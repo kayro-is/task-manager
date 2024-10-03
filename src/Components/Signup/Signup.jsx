545f9d29eacfc5335095fb528abca0d8ceb860d5
kayro-is <luffykosuki@gmail.com>
2024-10-03 13:00:09 +0200

    Ajouter la logique pour envoyer les données au back-end

--- src/Components/Signup/Signup.jsx
+++ src/Components/Signup/Signup.jsx
@@ -2,25 +2,55 @@
 import { useState } from 'react';
 import './Signup.scss'; // On va ajouter du CSS spécifique pour cette page
 
+
+
 function Signup() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    if (password === confirmPassword) {
-      console.log('Inscription réussie', { email, password });
-      // Envoyer les données au backend
-    } else {
-      console.log('Les mots de passe ne correspondent pas');
+    setErrorMessage('');
+    setSuccessMessage('');
+
+    if (password !== confirmPassword) {
+      setErrorMessage('les mots depasse ne correspondent pas');
+
+      return;
     }
-  };
+
+    try {
+      const response = await fetch('http://localhost:5000/api/signup', {
+        methode: 'POST',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (!response.ok) {
+        const errorData =  await response.json();
+        setErrorMessage(errorData.message || ' erreur lors de l\'inscription');
+
+        return;
+      }
+
+      setSuccessMessage('Inscription réussie ! Vous pouvez vous connecter.');
+      setEmail('');
+      setPassword('');
+      setConfirmPassword('');
+
+    }catch (error) {
+      setErrorMessage('Erreur lors de l\'inscription');
+      console.error('Erreur dans le handleSubmit de Signup', error);
+    }
+  }
+
 
   return (
     <div className="signup-page-container">
       <div className="logo-signup-container">
-        {/* Image ou logo fictif */}
         <img src="/Task.png" alt="Task Manager Logo" />
         <p>&quot;Simplifiez votre quotidien, une tâche à la fois.&quot;</p>
       </div>
@@ -29,6 +59,8 @@ function Signup() {
       <div className="signup-form-container">
         <h2>Hello, Créez votre compte</h2>
         <form onSubmit={handleSubmit} className="signup-form">
+          {errorMessage && <p className="error">{errorMessage}</p>}
+          {successMessage && <p className="success">{successMessage}</p>}
           <input
             type="email"
             placeholder="Entrez votre email"
